0b6f31766adffb1fb214969eb245580da877e058
Kotlyar Oleksandr <kotlyar7733@gmail.com>
2022-06-23 15:09:40 +0300

    first commit

---
+++ Assets/Scripts/TheEnd.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheEnd : MonoBehaviour
+{
+    public void QuitGame()
+    {
+        Debug.Log("The End!!!");
+        Application.Quit();
+    }
+}
